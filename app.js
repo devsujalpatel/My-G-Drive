06d2459a147ce4acec4a8b52f3bd9191ea1bc0f8
devsujalpatel <sujal7455@gmail.com>
2025-04-15 01:02:42 +0530

    adding css

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ import { open, readdir, readFile } from "fs/promises";
 import http from "http";
 
 const server = http.createServer(async (req, res) => {
-    if(req.url === '/storage/favicon.ico'){
-        const readStrem = createReadStream('favicon.ico')
-        readStrem.pipe(res)
-    }
+  if (req.url === "/favicon.ico" && req.url === "/style.css") {
+    return res.end("No Such file")
+  } 
   if (req.url === "/") {
-    serveDirectory(req, res)
+    serveDirectory(req, res);
   } else {
     try {
       const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
       const stats = await fileHandle.stat();
       if (stats.isDirectory()) {
-        serveDirectory(req, res)
+        serveDirectory(req, res);
       } else {
         const readStream = fileHandle.createReadStream();
         readStream.pipe(res);
@@ -26,16 +25,20 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-async function serveDirectory(req, res){
-    const itemList = await readdir(`./storage${req.url}`);
-    let dynamicHTML = "";
-    itemList.forEach((item) => {
-      dynamicHTML += `<li><a href=".${
-        req.url === '/' ? '' : req.url
-      }/${item}">${item}</a></li>`;
-    });
-    const htmlBoiler = await readFile("./boiler.html", "utf-8");
-    res.end(htmlBoiler.replace("${dynamicHTML}", dynamicHTML));
+async function serveDirectory(req, res) {
+  const itemList = await readdir(`./storage${req.url}`);
+  let dynamicHTML = "";
+  itemList.forEach((item) => {
+    dynamicHTML += `<li>${item}<a href=".${
+      req.url === "/" ? "" : req.url
+    }/${item}"> Open </a>
+        <a
+        href=".${req.url === "/" ? "" : req.url}/${item}"
+        >Download</a>
+      </li>`;
+  });
+  const htmlBoiler = await readFile("./public/boiler.html", "utf-8");
+  res.end(htmlBoiler.replace("${dynamicHTML}", dynamicHTML));
 }
 
 server.listen(80, "0.0.0.0", () => {
