18346e610f85a473e4d79c58236746d2743b00a9
devsujalpatel <sujal7455@gmail.com>
2025-04-15 14:02:26 +0530

    few changes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ import http from "http";
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/favicon.ico" && req.url === "/style.css") {
-    return res.end("No Such file")
-  } 
+    return res.end("No Such file");
+  }
   if (req.url === "/") {
     serveDirectory(req, res);
   } else {
     try {
-      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
+      const [url, queryStr] = req.url.split("?")
+      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
       const stats = await fileHandle.stat();
       if (stats.isDirectory()) {
         serveDirectory(req, res);
@@ -29,12 +30,13 @@ async function serveDirectory(req, res) {
   const itemList = await readdir(`./storage${req.url}`);
   let dynamicHTML = "";
   itemList.forEach((item) => {
-    dynamicHTML += `<li>${item}<a href=".${
+    dynamicHTML += `<li>${item}
+    <a href=".${
       req.url === "/" ? "" : req.url
-    }/${item}"> Open </a>
-        <a
-        href=".${req.url === "/" ? "" : req.url}/${item}"
-        >Download</a>
+    }/${item}?action=open"> Open </a>
+        <a href=".${
+          req.url === "/" ? "" : req.url
+        }/${item}?action=download">Download</a>
       </li>`;
   });
   const htmlBoiler = await readFile("./public/boiler.html", "utf-8");
