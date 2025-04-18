66ec1722df3e5e2d03d3da4860987fb391a9e6d6
devsujalpatel <sujal7455@gmail.com>
2025-04-19 02:04:22 +0530

    fixed the vulnerbility of system

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createWriteStream } from "fs";
 import { readdir, rename, rm, stat, mkdir } from "fs/promises";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -10,8 +11,9 @@ app.use(cors());
 
 // Read
 app.get("/directory/?*", async (req, res) => {
-  const { 0: dirname } = req.params;
+  const dirname = path.join("/", req.params[0]);
   const fullDirPath = `./storage/${dirname ? dirname : ""}`;
+ try {
   const filesList = await readdir(fullDirPath);
   const resData = [];
   for (const item of filesList) {
@@ -19,21 +21,25 @@ app.get("/directory/?*", async (req, res) => {
     resData.push({ name: item, isDirectory: stats.isDirectory() });
   }
   res.json(resData);
+ } catch (error) {
+  res.json({error: error.message})
+ }
 });
 
 app.post('/directory/*', async (req, res) => {
-  const { 0: dirname } = req.params;
+  const dirname = path.join("/", req.params[0]);
   try {
     await mkdir(`./storage/${dirname}`)
   res.json({message: "Directory create"})
   } catch (error) {
-    res.json({err: err.message})
+    res.json({message: "Cannot be empty"})
   }
 })
 
 // Create
 app.post("/files/*", (req, res) => {
-  const writeStream = createWriteStream(`./storage/${req.params[0]}`);
+  const filePath = path.join("/", req.params[0]);
+  const writeStream = createWriteStream(`./storage/${filePath}`);
   req.pipe(writeStream);
   req.on("end", () => {
     res.json({ message: "File Uploaded" });
@@ -43,23 +49,27 @@ app.post("/files/*", (req, res) => {
 // path travarsal vulnerbility
 
 app.get("/files/*", (req, res) => {
-  const { 0: filePath } = req.params;
+  const filePath = path.join("/", req.params[0]);
   if (req.query.action === "download") {
     res.set("Content-Disposition", "attachment");
   }
-  res.sendFile(`${import.meta.dirname}/storage/${filePath}`);
+  res.sendFile(`${import.meta.dirname}/storage/${filePath}`, (err) => {
+    if(err){
+      res.json({error: "File not found"})
+    }
+  });
 });
 
 // Update
 app.patch("/files/*", async (req, res) => {
-  const { 0: filePath  } = req.params;
+  const filePath = path.join("/", req.params[0]);
   await rename(`./storage/${filePath}`, `./storage/${req.body.newFilename}`);
   res.json({ message: "Renamed" });
 });
 
 // Delete
 app.delete("/files/*", async (req, res) => {
-  const { 0: filePath } = req.params;
+  const filePath = path.join("/", req.params[0]);
   try {
     await rm(`./storage/${filePath}`, {recursive: true});
     res.json({ message: "File Deleted Successfully" });
